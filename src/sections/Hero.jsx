a8232722348765e71ec34ddf37f46bb768b49fcc
soban-dev <sobantheprogrammer@gmail.com>
2024-12-12 18:09:20 +0500

    update.v1

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,10 +9,12 @@ const Hero = () => {
   const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
 
   return (
+    // Main Section
     <section
       id='home'
       className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
     >
+      {/* //Main text */}
       <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-coral-red'>
           Our Summer collections
@@ -43,7 +45,7 @@ const Hero = () => {
           ))}
         </div>
       </div>
-
+         {/* changeBigShoeImage */}
       <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
         <img
           src={bigShoeImg}
